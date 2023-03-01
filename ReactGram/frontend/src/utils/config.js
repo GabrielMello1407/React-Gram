644ef64185ab30c5f608e9fe3148d36d2aca92a1
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2023-03-01 17:37:39 -0300

    testes

--- ReactGram/frontend/src/utils/config.js
+++ ReactGram/frontend/src/utils/config.js
@@ -1,6 +1,6 @@
 //objeto de configuração das requisições do backend do sistema
 export const api = 'http://localhost:5000/api'
-export const uploads = 'http://localhost:5000/localhost'
+export const uploads = 'http://localhost:5000/uploads'
 
 export const requestConfig = (method, data , token = null, image = null) => {
     
